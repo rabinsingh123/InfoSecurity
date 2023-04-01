81d504bc4f9420bb08722392b1fa3203e8de64b7
Rabin Singh <rabinsingh540@gmail.com>
2023-03-31 22:11:17 -0400

    InfoSecurity code

---
+++ Documents/Gui/src/cipher.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class cipher extends JFrame {
+    private JTextField textField1;
+    private JFormattedTextField formattedTextField1;
+    private JButton encryptButton;
+    encryptButton.addActionListener(new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //your actions
+        }
+    });
+
+
+
+}
